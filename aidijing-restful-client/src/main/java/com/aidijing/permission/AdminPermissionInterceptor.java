d7b3383ed6a796a24dbb2c4ff3751df83d3ccc89
披荆斩棘 <yujunhao_8831@yahoo.com>
2017-06-20 10:35:37 +0800

    拦截器

--- aidijing-restful-client/src/main/java/com/aidijing/permission/AdminPermissionInterceptor.java
+++ aidijing-restful-client/src/main/java/com/aidijing/permission/AdminPermissionInterceptor.java
@@ -1,7 +1,5 @@
 package com.aidijing.permission;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
